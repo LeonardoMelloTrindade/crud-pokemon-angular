f9782bb438d352e8fb23b86e508d877d785b336c
LeonardoMelloTrindade <leo11madara@gmail.com>
2024-01-14 17:04:07 -0300

    first commit

---
+++ src/app/components/view-pokemon/view-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-pokemon.component.html',
+  styleUrl: './view-pokemon.component.css'
+})
+export class ViewPokemonComponent {
+
+}
